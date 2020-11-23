9a12fcc7831c59e8d1718d6976587a11611c5c9f
金戈宁 <jingening@gmail.com>
2020-11-23 15:42:07 +0800

    updeat: 添加默认props

--- src/components/whoIsUnder/Player.js
+++ src/components/whoIsUnder/Player.js
@@ -5,6 +5,13 @@ import global from "@/global";
 export default class Player extends Component {
   state = {};
 
+  static defaultProps = {
+    name: "空座位",
+    xxx1: "头像",
+    xxx2: "被投票的数量",
+    xxx3: "身份",
+  };
+
   componentDidMount() {}
 
   /**
